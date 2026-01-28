27460b2b332ef30ad4325c38d6479dd0d194ac0f
Rakib <rakibweb0@gmail.com>
2026-01-28 15:44:19 +0600

    display data

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
 import { getData } from "./data-access-layer/getData";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function Home() {
-  const data = await getData()
+  const data = await getData();
 
-  if (!data) return <div>No data</div>
+  if (!data) return <div>No data</div>;
 
   return (
-    <div>Hello {data[0]?.name} and your email is {data[0]?.email}</div>
+    <div className="max-w-2xl mx-auto border mt-20">
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="">Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead className="text-right">Phone</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell className="font-medium">{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell className="text-right">{user.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
